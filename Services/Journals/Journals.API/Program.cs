addffd8c8e24d1654ad1d9aac2f0b00442e338e9
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2026-05-28 18:00:12 +0530

    Build success

--- Services/Journals/Journals.API/Program.cs
+++ Services/Journals/Journals.API/Program.cs
@@ -1,33 +1,6 @@
-using FastEndpoints;
-using FastEndpoints.Swagger;
-using Journals.API;
-using Journals.Persistence;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services
-    .ConfigureApiOptions(builder.Configuration);
-
-#region Add Services
-
-builder.Services
-    .AddApiServices(builder.Configuration)
-    .AddPersistenceServices(builder.Configuration);
-
-#endregion
-
 var app = builder.Build();
 
-#region Use
-
-app
-    .UseSwagger()
-    .UseSwaggerUI()
-    .UseHttpsRedirection()
-    .UseRouting()
-    .UseFastEndpoints()
-    .UseSwaggerGen();
-
-#endregion
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
